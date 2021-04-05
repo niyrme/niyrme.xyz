fd2ea971bcb071ea4f51eb488d805fd7004a32e8
Niyrme <niyrme@niyrme.xyz>
2021-04-05 02:17:47 +0200

    Add blank about page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ export default function Navbar(): JSX.Element {
 	return (
 		<nav>
 			<Link href="/"><a>Home</a></Link>
+			<Link href="/about"><a>About</a></Link>
 		</nav>
 	);
 }
---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.scss";
+
+export default function About() {
+	return (
+		<div>
+			<h1 className={styles.title}>About</h1>
+		</div>
+	);
+}
