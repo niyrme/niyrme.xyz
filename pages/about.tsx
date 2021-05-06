a9414d68a92d304bf5c32259c1712ff6d2cee4f0
Niyrme <niyrme@niyrme.xyz>
2021-05-06 20:07:45 +0200

    Add about page and edit styles

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,11 @@
 import styles from "../styles/Home.module.scss";
+import stylesAbout from "../styles/About.module.scss";
 
 export default function About() {
 	return (
 		<div>
 			<h1 className={styles.title}>About</h1>
+			<div className={stylesAbout.about}></div>
 		</div>
 	);
 }
